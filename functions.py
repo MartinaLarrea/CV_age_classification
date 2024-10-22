840caa78512af6d4a9af0ec6211c530c88cf37f7
Martina Larrea <martinalarrea1112@gmail.co>
2024-10-22 00:56:45 -0300

    first commit

---
+++ functions.py
@@ -0,0 +1,38 @@
+def plotTraining(hist, epochs, typeData):
+    
+    if typeData=="loss":
+        plt.figure(1,figsize=(10,5))
+        yc=hist.history['loss']
+        xc=range(epochs)
+        plt.ylabel('Loss', fontsize=24)
+        plt.plot(xc,yc,'-r',label='Loss Training')
+    if typeData=="accuracy":
+        plt.figure(2,figsize=(10,5))
+        yc=hist.history['accuracy']
+        for i in range(0, len(yc)):
+            yc[i]=100*yc[i]
+        xc=range(epochs)
+        plt.ylabel('Accuracy (%)', fontsize=24)
+        plt.plot(xc,yc,'-r',label='Accuracy Training')
+    if typeData=="val_loss":
+        plt.figure(1,figsize=(10,5))
+        yc=hist.history['val_loss']
+        xc=range(epochs)
+        plt.ylabel('Loss', fontsize=24)
+        plt.plot(xc,yc,'--b',label='Loss Validate')
+    if typeData=="val_accuracy":
+        plt.figure(2,figsize=(10,5))
+        yc=hist.history['val_accuracy']
+        for i in range(0, len(yc)):
+            yc[i]=100*yc[i]
+        xc=range(epochs)
+        plt.ylabel('Accuracy (%)', fontsize=24)
+        plt.plot(xc,yc,'--b',label='Training Validate')
+        
+
+    plt.rc('xtick',labelsize=24)
+    plt.rc('ytick',labelsize=24)
+    plt.rc('legend', fontsize=18) 
+    plt.legend()
+    plt.xlabel('Number of Epochs',fontsize=24)
+    plt.grid(True)
